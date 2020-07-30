411301379d4bc9bb350bb4c33b4f7d06215b159b
Albert Arredondo <albert.arredondo@gesundheitscloud.de>
2020-07-30 09:02:22 +0200

    Project initialization.

---
+++ DomestikaTrailers/DomestikaTrailers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DomestikaTrailers
+//
+//  Created by Albert Arredondo Alfaro on 30/07/2020.
+//  Copyright © 2020 Albert Arredondo Alfaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
